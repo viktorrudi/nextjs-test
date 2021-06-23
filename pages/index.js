6d3625ce9b1bf7354900c6e49fcb1bec8821a267
Viktor Rudi <vrudi91@gmail.com>
2021-06-23 18:58:09 +0200

    test2

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>asdasdasdasd222</title>
+        <title>asdasdasdasd</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.jzzzzzz!</a>
         </h1>
 
         <p className={styles.description}>
